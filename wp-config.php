8a720641916482ae7f2df9170856107c0741249f
Kouetosine <kouetosine@gmail.com>
2023-05-01 23:34:03 +0100

    Configuration des fichiers wp-config.php et création du thème studi

--- wp-config.php
+++ wp-config.php
@@ -86,7 +86,7 @@ $table_prefix = 'gwp_';
  *
  * @link https://fr.wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* C’est tout, ne touchez pas à ce qui suit ! Bonne publication. */
 
